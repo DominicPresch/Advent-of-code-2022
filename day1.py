22db6e1831ee5c8bca6d0bdbacc8b89513570e88
Your Name <you@example.com>
2022-11-30 21:42:03 -0800

    day1advent2022

---
+++ day1.py
@@ -0,0 +1,21 @@
+with open("C:\\Users\\Dominic Presch\\Desktop\\adventofcode\\day1.txt") as x:
+    whale = 0
+    largestwhale = 0
+    secondlargestwhale = 0
+    thirdlargestwhale = 0
+    for inp in x:
+        inp = inp.strip()
+        if inp != "":
+            whale += int(inp)
+        else:
+            if whale > largestwhale:
+                thirdlargestwhale = secondlargestwhale
+                secondlargestwhale = largestwhale
+                largestwhale = whale
+            elif whale > secondlargestwhale:
+                thirdlargestwhale = secondlargestwhale
+                secondlargestwhale = whale
+            elif whale > thirdlargestwhale:
+                thirdlargestwhale = whale
+            whale = 0
+    print(largestwhale + secondlargestwhale + thirdlargestwhale)
